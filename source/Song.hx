1a66ca7b1a4b1065066bbbaa8d28e984d837bde2
DEMOLITIONDON96 <87618246+DEMOLITIONDON96@users.noreply.github.com>
2021-11-23 22:53:48 -0600

    ez

--- source/Song.hx
+++ source/Song.hx
@@ -104,6 +104,7 @@ class Song
 	{
 		var swagShit:SwagSong = cast Json.parse(rawJson).song;
 		swagShit.validScore = true;
+		if(swagShit.songKeys == null) swagShit.songKeys = 4;
 		return swagShit;
 	}
 }
